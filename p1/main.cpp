b727f3e240422d01b4661fb76a1e84406d675432
Stoozy <mahinsemail@gmail.com>
2021-02-04 14:29:27 -0500

    Problem 1 finished

---
+++ p1/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    unsigned sum=0;
+    for(int i=0; i<1000; ++i) {
+       if(i%3 == 0 || i%5==0)
+           sum+=i;
+    }
+    cout << "Sum is " << sum <<endl;
+
+    return 0;
+}
